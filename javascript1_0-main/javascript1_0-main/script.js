9ed4a3fa32c043ff96d7ac7366075f80b6431e4a
Kradoree <arkadiuszserafin7@gmail.com>
2024-10-03 09:21:33 +0200

    Zadanie 1

--- javascript1_0-main/javascript1_0-main/script.js
+++ javascript1_0-main/javascript1_0-main/script.js
@@ -1,3 +1,7 @@
 (function () {
   //TODO
+  const ex1 = document.getElementById("ex1_button")
+  ex1.addEventListener("click",(e)=>{
+    document.getElementById("ex1_content").innerHTML = "0,1,2,3,4,5,6,7,8,9";
+  })
 })();
